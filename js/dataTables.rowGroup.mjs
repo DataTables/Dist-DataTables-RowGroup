90b6cbda2b6990f2442404e6dc5596efc78be691
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-03-10 16:53:38 +0000

    c416dd010300788a1d9ced75c1a0385a8107ca1f
    Sync to source repo @c416dd010300788a1d9ced75c1a0385a8107ca1f

--- js/dataTables.rowGroup.mjs
+++ js/dataTables.rowGroup.mjs
@@ -1,16 +1,15 @@
-/*! RowGroup 1.3.0
- * ©2017-2022 SpryMedia Ltd - datatables.net/license
+/*! RowGroup 1.3.1
+ * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net';
 
 
-
 /**
  * @summary     RowGroup
  * @description RowGrouping for DataTables
- * @version     1.3.0
+ * @version     1.3.1
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
@@ -388,7 +387,7 @@ RowGroup.defaults = {
 };
 
 
-RowGroup.version = "1.3.0";
+RowGroup.version = "1.3.1";
 
 
 $.fn.dataTable.RowGroup = RowGroup;
