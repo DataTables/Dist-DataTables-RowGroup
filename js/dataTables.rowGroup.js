9e33d650fc32fdf21b61a2622c1d2fb9ed3790c6
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-10-26 13:55:20 +0100

    6e2b397326c4fccfb648e916f92774bace2e9c9b Update: Compatibility with DataTables 2
    Was using an old internal API before. Now uses public API which was
    exposed in 1.11. Version test updated
    
    3b8c10d119111241b78fee5ec509ac5e36906247 Update: Examples and documentation code examples all have consistent code styling
    
    Sync to source repo @3b8c10d119111241b78fee5ec509ac5e36906247

--- js/dataTables.rowGroup.js
+++ js/dataTables.rowGroup.js
@@ -1,4 +1,4 @@
-/*! RowGroup 1.4.1
+/*! RowGroup 1.5.0-dev
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -52,7 +52,7 @@ var DataTable = $.fn.dataTable;
 /**
  * @summary     RowGroup
  * @description RowGrouping for DataTables
- * @version     1.4.1
+ * @version     1.5.0-dev
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
@@ -69,8 +69,8 @@ var DataTable = $.fn.dataTable;
 
 var RowGroup = function (dt, opts) {
 	// Sanity check that we are using DataTables 1.10 or newer
-	if (!DataTable.versionCheck || !DataTable.versionCheck('1.10.8')) {
-		throw 'RowGroup requires DataTables 1.10.8 or newer';
+	if (!DataTable.versionCheck || !DataTable.versionCheck('1.11')) {
+		throw 'RowGroup requires DataTables 1.11 or newer';
 	}
 
 	// User and defaults configuration object
@@ -233,7 +233,7 @@ $.extend(RowGroup.prototype, {
 	 */
 	_group: function (level, rows) {
 		var fns = Array.isArray(this.c.dataSrc) ? this.c.dataSrc : [this.c.dataSrc];
-		var fn = DataTable.ext.oApi._fnGetObjectDataFn(fns[level]);
+		var fn = DataTable.util.get(fns[level]);
 		var dt = this.s.dt;
 		var group, last;
 		var data = [];
@@ -416,7 +416,7 @@ RowGroup.defaults = {
 	}
 };
 
-RowGroup.version = '1.4.1';
+RowGroup.version = '1.5.0-dev';
 
 $.fn.dataTable.RowGroup = RowGroup;
 $.fn.DataTable.RowGroup = RowGroup;
--- js/dataTables.rowGroup.mjs
+++ js/dataTables.rowGroup.mjs
@@ -1,4 +1,4 @@
-/*! RowGroup 1.4.1
+/*! RowGroup 1.5.0-dev
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -12,7 +12,7 @@ let $ = jQuery;
 /**
  * @summary     RowGroup
  * @description RowGrouping for DataTables
- * @version     1.4.1
+ * @version     1.5.0-dev
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
@@ -29,8 +29,8 @@ let $ = jQuery;
 
 var RowGroup = function (dt, opts) {
 	// Sanity check that we are using DataTables 1.10 or newer
-	if (!DataTable.versionCheck || !DataTable.versionCheck('1.10.8')) {
-		throw 'RowGroup requires DataTables 1.10.8 or newer';
+	if (!DataTable.versionCheck || !DataTable.versionCheck('1.11')) {
+		throw 'RowGroup requires DataTables 1.11 or newer';
 	}
 
 	// User and defaults configuration object
@@ -193,7 +193,7 @@ $.extend(RowGroup.prototype, {
 	 */
 	_group: function (level, rows) {
 		var fns = Array.isArray(this.c.dataSrc) ? this.c.dataSrc : [this.c.dataSrc];
-		var fn = DataTable.ext.oApi._fnGetObjectDataFn(fns[level]);
+		var fn = DataTable.util.get(fns[level]);
 		var dt = this.s.dt;
 		var group, last;
 		var data = [];
@@ -376,7 +376,7 @@ RowGroup.defaults = {
 	}
 };
 
-RowGroup.version = '1.4.1';
+RowGroup.version = '1.5.0-dev';
 
 $.fn.dataTable.RowGroup = RowGroup;
 $.fn.DataTable.RowGroup = RowGroup;
